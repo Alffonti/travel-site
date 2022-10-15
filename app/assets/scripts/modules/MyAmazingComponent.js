7b9d7a4241663dcd82eaed0e849a4a2d9d67b2e6
Alf Fonti <alffonti@gmail.com>
2022-10-15 13:27:02 -0300

    Added React to webpack

---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyAmazingComponent() {
+    return (
+      <div>
+        <h1 className="section-title section-title--blue">This is my Amazing React Component</h1>
+      </div>
+    )
+}
+
+export default MyAmazingComponent
